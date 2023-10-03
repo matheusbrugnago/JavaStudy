0048cb82730275c88ed1c086100fce32acf4abd9
Matheus Brugnago <106639493+matheusbrugnago@users.noreply.github.com>
2023-10-03 11:10:22 -0300

    Estudo de Java 03/10

---
+++ MeuPrimeiroProjetoJava/src/DeclaraVariaveis.java
@@ -0,0 +1,32 @@
+public class DeclaraVariaveis {
+    public static void main(String[] args) {
+        //var login= "Olá";
+        var password="hbkdghasdhasgdja";
+        System.out.println(password);
+
+        /*
+        Variáveis tipo:Texto(strings)
+        Variaveis tipo:Numeros(int)
+        Variaveis tipo: Numeros Fracionários(Double)
+        Variaveis tipo: Booleanas(verdadeiro | falso)
+         */
+        //TIPOS DE DADOS
+        //tipo   nomedareferencia  =  conteudo
+        String login= "Ola"; //Representação de Texto(ASCII)
+        int number1=10; //Representação de Números Inteiros
+        double percent=24.30; //Representação de Números fracionários
+        boolean isDriver=false; // Representação booleana(estado verdadeiro, falso)
+
+        System.out.println(login);
+        System.out.println(number1);
+        System.out.println(percent);
+        System.out.println(isDriver);
+
+        //variaveis
+        // São espaços de memória alocado( Representado por tipos)
+
+        int number; //declarando variavel
+        number=55;
+        System.out.println(number+10);
+    }
+}
---
+++ MeuPrimeiroProjetoJava/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,14 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args){
+        System.out.println(2*5*3);
+        System.out.println(10/5);
+        System.out.println(9+3);
+        //Comentários são importantes
+        //Ponto de entrada- Entrypoint-> Ponto que todos os sistemas procuram no seu
+        //projeto para dar início
+        //Métodos: São instruções que define para programar executar-Exemplo:
+        //Instrução: Chamada System.out.println("")
+        //Propriedades/variaveis: Onde Armazenar Dados para manipulá-los no futuro
+    }
+}
